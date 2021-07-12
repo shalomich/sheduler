e2c2ecaa8231b452ab273e1441aa97162bfa1ec1
shalomich <shalo6947@gmail.com>
2021-07-12 21:47:58 +0700

    create database configurations for model

---
+++ Sheduler/DbConfigs/UserDbConfig.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sheduler.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sheduler.DbConfigs
+{
+    public class UserDbConfig : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+
+            builder.Property(account => account.Email);
+            builder.Property(account => account.Email).IsRequired();
+            builder.HasIndex(account => account.Email).IsUnique();
+
+            builder.Property(account => account.Password).IsRequired();
+
+            builder.HasIndex(account => account.PhoneNumber).IsUnique();
+
+            builder.Property(user => user.Role).HasConversion<string>();
+        }
+    }
+}
---
+++ Sheduler/DbConfigs/VacationRequestDbConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sheduler.Model.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sheduler.DbConfigs
+{
+    public class VacationRequestDbConfig : IEntityTypeConfiguration<VacationRequest>
+    {
+        public void Configure(EntityTypeBuilder<VacationRequest> builder)
+        {
+            builder.Property(request => request.VacationType).IsRequired();
+            builder.Property(request => request.IsDateChangeable).IsRequired();
+        }
+    }
+}
