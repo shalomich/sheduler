cecbfc5b368032410bd477929d4e28e83a08a108
shalomich <shalo6947@gmail.com>
2021-07-12 23:08:41 +0700

    create class AuthController

--- Sheduler/Controllers/AuthController.cs
+++ Sheduler/Controllers/AuthController.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Sheduler.Model;
+using Sheduler.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using static Sheduler.RequestHandlers.Auth.LoginHandler;
 
 namespace Sheduler.Controllers
 {
@@ -15,61 +18,20 @@ namespace Sheduler.Controllers
     [Route("auth")]
     public class AuthController : Controller
     {
-        private ApplicationContext Context { get; }
-        private AuthOptions Options { get; }
+        private IMediator Mediator { get; }
 
-        public AuthController(ApplicationContext context, IOptions<AuthOptions> options)
+        public AuthController(IMediator mediator)
         {
-            Context = context ?? throw new ArgumentNullException(nameof(context));
-            Options = options.Value ?? throw new ArgumentNullException(nameof(options));
+            Mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         }
 
         [HttpPost]
         [Route("login")]
-        public IActionResult Login([FromBody] string email,[FromBody] string password)
+        public async Task<IActionResult> Login(LoginViewModel loginModel)
         {
-            var identity = GetIdentity(email, password);
-            if (identity == null)
-            {
-                return BadRequest();
-            }
+            string token = await Mediator.Send(new LoginCommand(loginModel));
 
-
-            var now = DateTime.UtcNow;
-
-            var jwt = new JwtSecurityToken(
-                    issuer: Options.Issuer,
-                    audience: Options.Audience,
-                    notBefore: DateTime.UtcNow,
-                    claims: identity.Claims,
-                    expires: now.Add(TimeSpan.FromMinutes(Options.Lifetime)),
-                    signingCredentials: new SigningCredentials(Options.GenerateSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
-            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
-            
-            return Json(encodedJwt);
-        }
- 
-        private ClaimsIdentity GetIdentity(string email, string password)
-        {
-            try
-            {
-                User user = Context.Users
-                    .Single(user => user.Email == email && user.Password == password);
-                
-                var claims = new List<Claim>
-                {
-                    new Claim(ClaimsIdentity.DefaultNameClaimType, user.Id.ToString()),
-                    new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role.ToString())
-                };
-                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "Token",
-                    ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
-                
-                return claimsIdentity;
-            }
-            catch(Exception)
-            {
-                return null;
-            }
+            return new JsonResult(token);    
         }
     }
     
--- Sheduler/Startup.cs
+++ Sheduler/Startup.cs
@@ -1,3 +1,5 @@
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -5,6 +7,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Sheduler.Controllers;
+using Sheduler.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +30,30 @@ namespace Sheduler
         {
             string connectionString = Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
+
+            services.Configure<AuthOptions>(Configuration.GetSection(nameof(AuthOptions)));
+
+            AuthOptions authOptions = Configuration.GetSection(nameof(AuthOptions)).Get<AuthOptions>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidIssuer = authOptions.Issuer,
+                            ValidateAudience = true,
+                            ValidAudience = authOptions.Audience,
+                            ValidateLifetime = true,
+                            IssuerSigningKey = authOptions.GenerateSymmetricSecurityKey(),
+                            ValidateIssuerSigningKey = true,
+                        };
+                    });
+            services.AddAuthorization();
+            services.AddControllers();
+
+            services.AddMediatR(typeof(AuthController).Assembly);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -36,8 +65,12 @@ namespace Sheduler
             
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapDefaultControllerRoute();
                 endpoints.MapControllers();
             });
         }
