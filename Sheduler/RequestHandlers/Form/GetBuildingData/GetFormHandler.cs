5e6491d7644b78e508fe8a458d49327395ae19e8
shalomich <shalo6947@gmail.com>
2021-07-13 18:45:08 +0700

    create class GetFormHandler

---
+++ Sheduler/RequestHandlers/Form/GetBuildingData/GetFormHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Sheduler.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sheduler.RequestHandlers.Form.GetBuildingData
+{
+    public class GetFormHandler : IRequestHandler<GetFormQuery, IEnumerable<object>>
+    {
+        private ToFormConverter Converter { get; }
+
+        public GetFormHandler(ToFormConverter converter)
+        {
+            Converter = converter ?? throw new ArgumentNullException(nameof(converter));
+        }
+
+        public Task<IEnumerable<object>> Handle(GetFormQuery request, CancellationToken cancellationToken)
+        {
+            return Task.Run(() => Converter.Convert(request.ModelType));
+        }
+    }
+}
