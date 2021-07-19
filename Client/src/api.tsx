629519ca0b798dec55a0f0f9bcdcd7e0e1053cae
shalomich <shalo6947@gmail.com>
2021-07-19 11:48:38 +0700

    create function api

---
+++ Client/src/api.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export enum SuccesStatus {
+    OK = 200,
+    Created = 201,
+    NoContent = 204
+}
+
+const api = (uri : string, options : any = {},successStatus : SuccesStatus = SuccesStatus.OK) => {
+
+    options.headers = {
+        'Accept': 'application/json, text/plain',
+        'Content-Type': 'application/json;charset=UTF-8'
+    };
+
+    options.body = JSON.stringify(options.body)
+  
+    return new Promise((resolve, reject) => {
+      fetch(uri, options)
+        .then(response => {
+          if (response.status === successStatus)
+            return resolve(response.json());
+          else 
+            return reject(response.json()); 
+        });
+    });
+}
+
+export default api
--- Client/src/components/forms/LoginForm.tsx
+++ Client/src/components/forms/LoginForm.tsx
@@ -1,32 +1,33 @@
 import React from 'react'
-import { FormType } from '../../HOCs/GenerateForm'
 import axios from 'axios'
 import {UseAuthorizedContext} from '../Account'
 import { UseErrorContext } from '../ErrorPresenter'
-
-
-type LoginFormType = {
-    uri : string,
-    Form : React.FC<FormType>
-}
+import {ActionFormType} from '../pages/FormPage'
+import api from '../../api'
 
 type LoginFormData = {
     email : string,
     password : string
 }
 
-const LoginForm : React.FC<LoginFormType> = ({uri, Form}) => {
+
+
+const LoginForm : React.FC<ActionFormType> = ({uri, Form}) => {
 
     const {UpdateToken} = UseAuthorizedContext()
-    const {SetErrors} = UseErrorContext()
+    const {ShowErrors} = UseErrorContext()
 
     const Login = (formData : LoginFormData) => {
-        axios.post(uri,formData)
-            .then(responce => UpdateToken(responce.data))
-            .catch(failure =>{
-               console.log(failure);
-            });
-            
+        console.log(formData)
+        api(uri, {
+            method: 'POST', 
+            body: formData
+        })
+        .then(responce => UpdateToken(responce as string))
+        .catch(async failure =>{
+            const errorResponce = await failure
+            ShowErrors(errorResponce.errors)
+        });  
     }
 
     return <Form sendHandler={Login}/>
