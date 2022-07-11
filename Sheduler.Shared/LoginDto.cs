2ff5df86a955a8c4e08d6396a41afaf4b61c7700
Roman Simernin <shalomich4769@gmail.com>
2022-07-11 22:06:21 +0700

    feat: create login command #1

---
+++ Sheduler.Shared/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sheduler.Shared;
+public record LoginDto
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; init; }
+
+    [Required]
+    public string Password { get; init; }
+}
+
---
+++ Sheduler.UseCases/Auth/LoginCommand.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using RestSharp;
+using Sheduler.Abstractions;
+using Sheduler.Shared;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sheduler.UseCases.Auth;
+
+public record LoginCommand(string Email, string Password) : IRequest;
+internal class LoginCommandHandler : AsyncRequestHandler<LoginCommand>
+{
+    private readonly IAuthTokenProvider authTokenProvider;
+    private readonly RestClient restClient;
+
+    public LoginCommandHandler(
+        IAuthTokenProvider authTokenProvider,
+        RestClient restClient)
+    {
+        this.authTokenProvider = authTokenProvider;
+        this.restClient = restClient;
+    }
+
+    protected override async Task Handle(LoginCommand request, CancellationToken cancellationToken)
+    {
+        var loginDto = new LoginDto
+        {
+            Email = request.Email,
+            Password = request.Password,
+        };
+
+        var restRequest = new RestRequest("", Method.Post)
+            .AddBody(loginDto);
+
+        var response = await restClient.ExecuteAsync<string>(restRequest, cancellationToken);
+
+        authTokenProvider.AccessToken = response.Content;
+    }
+}
+
